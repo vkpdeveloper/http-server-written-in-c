be77287e27517fa879d41b06282e786173935c9a
vkpdeveloper <vaibhavpathak9984@gmail.com>
2024-07-04 00:11:44 +0530

    some updates

--- app/server.c
+++ app/server.c
@@ -161,7 +161,6 @@ void reply(int client_fd, http_request *request) {
     char *copied_request_path = strdup(request->path);
 
     char *file_path = extract_the_last_token((char *)copied_request_path);
-    printf("going to read file %s\n", file_path);
     char *file_content = read_file(file_path);
     if (file_content == NULL) {
       reply_with_404(client_fd);
@@ -305,10 +304,10 @@ int sizeof_header(header **headers) {
 char *read_file(char *file_path) {
   char *abs_file_path = malloc(strlen(file_path) + 5);
   sprintf(abs_file_path, "%s%s", base_dir_path, file_path);
-  printf("path:: %s\n", abs_file_path);
-  FILE *fp;
-  fp = fopen(abs_file_path, "r");
+  printf("path: %s\n", abs_file_path);
+  FILE *fp = fopen(abs_file_path, "r");
   if (fp == NULL) {
+    perror("failed fp");
     free(abs_file_path);
     return NULL;
   }
